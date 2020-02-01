024d1ca5888166d0809df23495374e694dcb1be3
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-01 17:28:28 +0530

    Create stem.py

---
+++ words/english/stem.py
@@ -0,0 +1,13 @@
+
+from nltk.stem.porter import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+stemmer = PorterStemmer() 
+  
+# stem words in the list of tokenised words 
+def stem_words(text): 
+    word_tokens = word_tokenize(text) 
+    stems = [stemmer.stem(word) for word in word_tokens] 
+    return stems 
+  
+text = 'data science uses scientific methods algorithms and many types of processes'
+stem_words(text) 
